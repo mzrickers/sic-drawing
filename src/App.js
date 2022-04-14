753c34d95cd727616b3ba07cf0b888455893c17b
Zach Rickers <mzrickers@gmail.com>
2022-04-14 10:53:31 +0200

    created canvas component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Canvas from './Components/Canvas';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      
+        <Canvas 
+            width={700}
+            height={500}
+        />
+     
     </div>
   );
 }
---
+++ src/Components/Canvas.js
@@ -0,0 +1,19 @@
+const Canvas = ({
+  width,
+  height
+}) => {
+
+    return (
+      <canvas
+          width={width}
+          height={height}
+          style={canvasStyle}
+      />
+    );
+}
+
+export default Canvas;
+
+const canvasStyle = {
+  border: "1px solid black"
+}
